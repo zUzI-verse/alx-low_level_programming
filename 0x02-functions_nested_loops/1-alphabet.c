7f31b7fd2fa269cd90d61cc1d16d907657cd685b
zUzI-verse <zuziobioha@gmail.com>
2022-07-06 10:47:26 -0700

    printalphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet -> print the lower case alphabet
+ */
+
+void print_alphabet(void)
+{
+
+	int k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		_putchar(k);
+	}
+	_putchar('\n');
+}
