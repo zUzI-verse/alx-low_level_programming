347e89b458985cbd1d7a058405ee794fc8455573
zUzI-verse <zuziobioha@gmail.com>
2022-07-06 15:19:54 -0700

    Alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetical letter
+ * @c: a charter to be checked on
+ * Return: returns 0 or 1 depending on condition
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && C <= 'Z'));
+}
