3c96b042bdaea24a5fffb30e22ffd4a350ba12b2
zUzI-verse <zuziobioha@gmail.com>
2022-07-06 17:11:54 -0700

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs -> return absolute valueof a number
+ * @n: parameter
+ * Return: absolute value off a number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,4 +13,5 @@ int _isalpha(int c);
 
 int print_sign(int n);
 
+int _abs(int);
 #endif
